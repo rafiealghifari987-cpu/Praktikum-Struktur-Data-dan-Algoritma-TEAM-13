8c86100878d3ff3a3645f357f10c674b6906283e
Rafie <rafiealghifari987@gmail.com>
2026-05-27 18:18:36 +0700

    menambahkan deklarasi string sorting di header

--- sorting.h
+++ sorting.h
@@ -1,11 +1,17 @@
 #ifndef SORTING_H
 #define SORTING_H
 
+
 void bubbleSort(int arr[], int n);
 void insertionSort(int arr[], int n);
 void selectionSort(int arr[], int n);
-
 void mergeSort(int arr[], int left, int right);
 void quickSort(int arr[], int low, int high);
 void shellSort(int arr[], int n);
+
+
+void mergeSortString(char arr[][100], int left, int right);
+void quickSortString(char arr[][100], int low, int high);
+void shellSortString(char arr[][100], int n);
+
 #endif
