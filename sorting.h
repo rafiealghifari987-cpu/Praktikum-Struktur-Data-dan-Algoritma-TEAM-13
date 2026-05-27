0b6b55128b1b5001b227ba4e2b24299ce61b89c2
Rafie <rafiealghifari987@gmail.com>
2026-05-27 17:25:34 +0700

    Menambahkan implementasi algoritma merge sort

--- sorting.h
+++ sorting.h
@@ -5,4 +5,7 @@ void bubbleSort(int arr[], int n);
 void insertionSort(int arr[], int n);
 void selectionSort(int arr[], int n);
 
+void mergeSort(int arr[], int left, int right);
+void quickSort(int arr[], int low, int high);
+void shellSort(int arr[], int n);
 #endif
